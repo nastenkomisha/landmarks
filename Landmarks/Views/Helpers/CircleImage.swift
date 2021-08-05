eaf90b493f7e2a2d405ba1fb1e84cbfe1e8c99cd
nastenkomisha <nastenkomisha@gmail.com>
2021-08-05 18:44:42 +0300

    Handling User Input

---
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Mikhail Nastenko on 03.08.2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
